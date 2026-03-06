376135d715d23c1c58dac6f6d2776e9858868426
Pravesh Rawat <65531568+prawat20@users.noreply.github.com>
2026-03-07 00:18:11 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -29,33 +29,3 @@ form.addEventListener("submit", async (e) => {
 
 });
 
-const form = document.getElementById("signup-form-2");
-
-form.addEventListener("submit", async (e) => {
-
-  e.preventDefault();
-
-  const email = document.getElementById("email").value;
-
-  try {
-
-    const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbzEBB-zKBRPyli3-v3PemLfQcyR11Dr8I5FEd5yr5uy93SxsVGGHw37jEPUy77xexI/exec",
-      {
-        method: "POST",
-        body: new URLSearchParams({
-          email: email
-        })
-      }
-    );
-
-    alert("✅ You're on the PlotTwyst waitlist!");
-    form.reset();
-
-  } catch (error) {
-
-    alert("❌ Something went wrong. Please try again.");
-
-  }
-
-});
